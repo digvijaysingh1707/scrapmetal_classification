7b945074cc94cea91bc3d48554b57175513520df
digvijaysingh1707 <digvijays17072003@gmail.com>
2024-07-17 15:12:19 +0530

    Added model files

---
+++ model.py
@@ -0,0 +1,205 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+import torchvision
+from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+
+from torchinfo import summary
+
+import matplotlib.pyplot as plt
+
+import os
+from tqdm.auto import tqdm
+from pathlib import Path
+
+
+image_path = Path("Grade_wise_scrap_photos")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def walk_through_dir(dir_path):
+    """
+    Walks through dir_path returning its contents.
+    Args:
+      dir_path (str or pathlib.Path): target directory
+
+    Returns:
+      A print out of:
+        number of subdiretories in dir_path
+        number of images (files) in each subdirectory
+        name of each subdirectory
+    """
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(
+            f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'."
+        )
+
+
+class TinyVGG(nn.Module):
+    """
+    Model architecture copying TinyVGG from:
+    https://poloclub.github.io/cnn-explainer/
+    """
+
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=input_shape,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units * 160 * 160, out_features=output_shape),
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
+
+
+def train_step(
+    model: torch.nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+):
+    model.train()
+
+    train_loss, train_acc = 0, 0
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
+
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+def train(
+    model: torch.nn.Module,
+    train_dataloader: torch.utils.data.DataLoader,
+    optimizer: torch.optim.Optimizer,
+    loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+    epochs: int = 5,
+):
+
+    results = {"train_loss": [], "train_acc": []}
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+        )
+
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} "
+        )
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+
+    return results
+
+
+if __name__ == "__main__":
+    # Transformer for image
+
+    data_transform = transforms.Compose(
+        [
+            transforms.Resize(size=(640, 640)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+        ]
+    )
+
+    # data_transform = transforms.Compose(
+    #     [
+    #         transforms.Resize((620, 640)),
+    #         transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    #         transforms.ToTensor(),
+    #     ]
+    # )
+
+    train_data = datasets.ImageFolder(
+        root=image_path,
+        transform=data_transform,
+    )
+    print(f"Train data:\n{train_data}")
+    class_names = train_data.classes
+    class_dict = train_data.class_to_idx
+
+    train_dataloader = DataLoader(
+        dataset=train_data,
+        batch_size=32,
+        num_workers=1,
+        shuffle=True,
+    )
+    img, label = next(iter(train_dataloader))
+    print(f"Image shape: {img.shape} -> [batch_size, color_channels, height, width]")
+    print(f"Label shape: {label.shape}")
+
+    torch.manual_seed(42)
+    model_0 = TinyVGG(
+        input_shape=3,
+        hidden_units=10,
+        output_shape=len(train_data.classes),
+    ).to(device)
+
+    print(summary(model_0, input_size=[32, 3, 640, 640]))
+
+    NUM_EPOCHS = 50
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+    model_0_results = train(
+        model=model_0,
+        train_dataloader=train_dataloader,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        epochs=NUM_EPOCHS,
+    )
